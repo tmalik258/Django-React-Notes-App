c96e8006e435390f42c420c853bf32bbf4b35c68
Talha Malik <talhamalik25.tm@gmail.com>
2023-09-24 16:23:52 +0500

    Added all api routes

--- api/views.py
+++ api/views.py
@@ -50,7 +50,7 @@ def getRoutes(request):
 
 @api_view(["GET"])
 def getNotes(request):
-    notes = Note.objects.all()
+    notes = Note.objects.all().order_by("-updated")
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
@@ -64,3 +64,42 @@ def getNote(request, pk):
         return Response(content, status=status.HTTP_204_NO_CONTENT)
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def updateNote(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+        data = request.data
+    except ObjectDoesNotExist:
+        content = {'please move along': 'nothing to see here'}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
+
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+        note.delete()
+    except ObjectDoesNotExist:
+        content = { 'please move along': 'nothing to see here' }
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
+    
+    return Response('Note was Deleted!')
